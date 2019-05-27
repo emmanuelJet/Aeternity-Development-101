4a0e95f627a4db99e60a361abef86d4890eb9195
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2019-05-27 18:28:26 +0100

    Payment Fixed

--- js/payment.js
+++ js/payment.js
@@ -4,11 +4,31 @@ var paymentArray = [];
 var paymentsLength = 0;
 
 
-function renderMemes() {
+function renderPayments() {
   var paymentJs = $('#paymentJs').html();
   Mustache.parse(paymentJs);
   var rendered = Mustache.render(paymentJs, {paymentArray});
   $('#paymentList').html(rendered);
+}
+
+async function callStatic(func, args, types) {
+  const calledGet = await client.contractCallStatic(contractAddress,
+  'sophia-address', func, {args}).catch(e => console.error(e));
+
+  const decodedGet = await client.contractDecodeData(types,
+  calledGet.result.returnValue).catch(e => console.error(e));
+
+  return decodedGet;
+}
+
+async function contractCall(func, args, value, types) {
+  const calledSet = await client.contractCall(contractAddress, 'sophia-address',
+  contractAddress, func, {args, options: {amount:value}}).catch(async e => {
+    const decodedError = await client.contractDecodeData(types,
+    e.returnValue).catch(e => console.error(e));
+  });
+
+  return
 }
 
 window.addEventListener('load', async () => {
@@ -16,21 +36,33 @@ window.addEventListener('load', async () => {
 
   client = await Ae.Aepp();
 
-  const calledGet = await client.contractCallStatic(contractAddress, 'sophia-address', 'getPaymentsLength', {args: '()'}).catch(e => console.error(e));
-  console.log('calledGet', calledGet);
-  const decodedGet = await client.contractDecodeData('int', calledGet.result.returnValue).catch(e => console.error(e));
-  console.log('decodedGet', decodedGet.value);
+  const getPaymentsLength = await callStatic('getPaymentsLength','()','int');
+  paymentsLength = getPaymentsLength.value;
 
-  renderMemes();
+  for (let i = 1; i <= paymentsLength; i++) {
+    const payment = await callStatic('getPayment',`(${i})`,'(address, address, string, int, string, int)');
+
+    paymentArray.push({
+      name: payment.value[2].value,
+      matric: payment.value[3].value,
+      payType: payment.value[4].value,
+      amount: payment.value[5].value,
+      index: i,
+    })
+  }
+
+  renderPayments();
 
   $('#loader').hide();
 });
 
 $('#registerBtn').click(async function(){
-  var name = ($('#paymentName').val()),
+  const name = ($('#paymentName').val()),
       matric = ($('#paymentMatric').val()),
       paymentType = ($('#paymentType').val()),
       amount = ($('#paymentAmount').val());
+
+  await contractCall('makePayment',`("${name}","${matric}","${paymentType}","${amount}")`,`("${amount}")`,'(int)');
 
   paymentArray.push({
     name: name,
@@ -39,5 +71,5 @@ $('#registerBtn').click(async function(){
     amount: amount,
     index: paymentArray.length + 1,
   })
-  renderMemes();
+  renderPayments();
 });
