91d001df2b60a9bf6e1e0db3256d59eff0316403
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2019-06-03 16:09:40 +0100

    Contract Bug Fixed

--- js/payment.js
+++ js/payment.js
@@ -19,14 +19,14 @@ const contractSource = `
       let payment = { studentAddress = Call.caller, schoolAddress = Call.origin, name = name', matric = matric', payType = payType', amount = amount'}
       let index = getPaymentsLength() + 1
       Chain.spend(payment.schoolAddress, Call.value)
-      "put(state{ payments[index] = payment, paymentsLength = index })"
+      put(state{ payments[index] = payment, paymentsLength = index })
     public function getPayments(index : int) : payment =
       switch(Map.lookup(index, state.payments))
         None    => abort("There was no payment with this index registered.")
         Some(x) => x
 `;
 
-const contractAddress = 'ct_cDvJebib46QdG6GxEmAW4J7DDHSvWcYD5SAwuai6P8r35e5Gy';
+const contractAddress = 'ct_7d9P3Ss6d8Z2F3kk87bB4oGH8K3EvFUH7YbFdRFPLz5hUQ1jT';
 var client = null;
 var paymentArray = [];
 var paymentsLength = 0;
