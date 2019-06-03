7003eaaf82d744bc53061993aaa9d55e8d98c8d1
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2019-06-03 16:25:06 +0100

    Bug Fixed

--- js/payment.js
+++ js/payment.js
@@ -61,7 +61,7 @@ window.addEventListener('load', async () => {
   paymentsLength = await callStatic('getPaymentsLength', []);
 
   for (let i = 1; i <= paymentsLength; i++) {
-    const payment = await callStatic('getpayment', [i]);
+    const payment = await callStatic('getpayments', [i]);
     paymentArray.push({
       name: payment.name,
       matric: payment.matric,
